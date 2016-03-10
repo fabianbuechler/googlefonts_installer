18238a047f59a7a318fa1d0437761dcf24c856a5
Fabian Büchler <fabian@buechler.io>
2016-03-10 22:58:08 +0100

    Clean up broken symlinks after uninstalling fonts.

--- googlefonts_installer.py
+++ googlefonts_installer.py
@@ -35,36 +35,39 @@ class GoogleFontInstaller:
         self.write_sparse_info(self.work_dir, fonts)
         self.update_git(self.work_dir)
         self.link_fonts(self.fonts_dir, self.work_dir, fonts)
+        self.clean_broken_links(self.fonts_dir)
 
     def read_config(self, config_path):
         """Read list of fonts to install from config file."""
+        print("Read fonts configuration")
         fonts = []
         with open(config_path, mode='r', encoding='utf-8') as conf:
             fonts = [line.rstrip('\n') for line in conf]
         print(
-            "Fonts to be installed:\n" +
+            "  Fonts to be installed:\n" +
             '\n'.join('    ' + font for font in fonts))
         return fonts
 
     def init_git(self, work_dir, repo_url):
         """Init git repository if required."""
+        print("Initialize git repository")
         # Create work_dir.
         if not os.path.exists(work_dir):
-            print("Create working directory at " + work_dir)
+            print("  Create working directory at " + work_dir)
             os.makedirs(work_dir)
         # Check for existing .git dir.
         if not os.path.exists(os.path.join(work_dir, '.git')):
             os.chdir(work_dir)
             # Init empty repository.
-            print("Init empty Git repository")
+            print("  Init empty Git repository")
             subprocess.check_call(['git', 'init'])
-            print("Enable sparse checkout")
+            print("  Enable sparse checkout")
             subprocess.check_call(
                 ['git', 'config', 'core.sparseCheckout', 'true'])
-            print("Add remote to github")
+            print("  Add remote to github")
             subprocess.check_call(['git', 'remote', 'add', 'origin', repo_url])
         else:
-            print("Git repository exists")
+            print("  Git repository exists")
 
     def write_sparse_info(self, work_dir, fonts):
         """Write ``.git/info/sparse-checkout`` configuration."""
@@ -76,25 +79,41 @@ class GoogleFontInstaller:
 
     def update_git(self, work_dir):
         """Pull changes and run ``read-tree`` to update sparse checkout."""
+        print("Update git repository")
         os.chdir(work_dir)
-        print("Pull changes from github")
+        print("  Pull changes from github")
         subprocess.check_call(['git', 'pull', '--depth=1', 'origin', 'master'])
-        print("Update working tree")
+        print("  Update working tree")
         subprocess.check_call(['git', 'read-tree', '--reset', '-u', 'HEAD'])
 
     def link_fonts(self, fonts_dir, work_dir, fonts):
         """Create symlinks for all ``fonts`` in ``fonts_dir``."""
+        print("Create symlinks for installed fonts")
         for font in fonts:
             # Make sure we have a trailing slash.
             font = font.rstrip('/') + '/'
             source = os.path.join(work_dir, font)
             font_name = font.rsplit('/', 2)[-2]
             target = os.path.join(fonts_dir, font_name)
-            # Recreate links in any case.
-            if os.path.exists(target):
-                os.remove(target)
-            print("Symlink {} to {}".format(target, source))
-            os.symlink(source, target)
+            # Skip existing symlinks.
+            if not os.path.exists(target):
+                print("  Symlink {} to {}".format(
+                    reduceuser(target), reduceuser(source)))
+                os.symlink(source, target)
+            else:
+                print("  Skip existing symlink {} to {}".format(
+                    reduceuser(target), reduceuser(source)))
+
+    def clean_broken_links(self, fonts_dir):
+        """Remove broken symlinks from ``fonts_dir`` for uninstalled fonts."""
+        print("Clean up symlinks for uninstalled fonts")
+        # dirs are the symlinks to the work_dir directories.
+        for entry in os.listdir(fonts_dir):
+            path = os.path.join(fonts_dir, entry)
+            # Check for broken symlinks.
+            if not os.path.exists(path) and os.path.lexists(path):
+                os.remove(path)
+                print("  Remove symlink {}.".format(reduceuser(path)))
 
 
 def main(argv=None):
@@ -136,3 +155,9 @@ def main(argv=None):
 
     except KeyboardInterrupt:
         parser.exit(1, "\nExecution cancelled.")
+
+
+def reduceuser(path):
+    """Replace absolute ``$HOME`` value in ``path`` with ``~``."""
+    home = os.path.expanduser('~')
+    return path.replace(home, '~', 1)
